c3f06bef3abd96414d6b261b4d0a02229d5151f9
VADLURIVIDHATH <111587094+VADLURIVIDHATH@users.noreply.github.com>
2023-07-31 20:50:14 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBAJ2ZPxz7gCHtkF0hTLtIX0TQL_j9uPc0",
+  authDomain: "spectogram-8c9a2.firebaseapp.com",
+  projectId: "spectogram-8c9a2",
+  storageBucket: "spectogram-8c9a2.appspot.com",
+  messagingSenderId: "330464649501",
+  appId: "1:330464649501:web:8166e25b0e9d95009181f4"
+};
+  
+  
